3ce6e1a2e532d8daa2b6e3a58365edf69b43557a
Thuto <ThutoSiya@gmail.com>
2026-04-03 17:46:29 +0200

    Insert African Teams into Dim_Team

---
+++ sql/dml/01_Insert_Dim_Team.sql
@@ -0,0 +1,18 @@
+USE WorldCupDW;
+GO
+
+-- AFRICA (CAF)
+
+INSERT INTO Dim_Team(Team_Name,Team_Nickname,Confederation,Region,Country_Code)
+VALUES
+('South Africa','Bafana Bafana','CAF','Africa','RSA'),
+('Morocco','Atlas Lions','CAF','Africa','MAR'),
+('Senegal','Lions of Teranga','CAF','Africa','SEN'),
+('Tunisia', 'Eagles of Carthage', 'CAF', 'Africa', 'TUN'),
+('Algeria', 'Desert Foxes', 'CAF', 'Africa', 'ALG'),
+('Egypt', 'Pharaohs', 'CAF', 'Africa', 'EGY'),
+('Côte d''Ivoire', 'Elephants', 'CAF', 'Africa', 'CIV'),
+('Ghana', 'Black Stars', 'CAF', 'Africa', 'GHA'),
+('Cabo Verde', 'Blue Sharks', 'CAF', 'Africa', 'CPV'),
+('Democratic Republic of the Congo', 'Leopards', 'CAF', 'Africa', 'COD');
+GO
